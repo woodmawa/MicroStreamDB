8228007c5cec2d760e96eadf215a7ee0f583b9f0
woodmawa <will.woodman@btinternet.com>
2022-03-17 15:15:39 +0000

    add generator and types

--- src/test/groovy/scripts/tryDatabase.groovy
+++ src/test/groovy/scripts/tryDatabase.groovy
@@ -8,7 +8,7 @@ def saveRes = db.save ( "hello world")
 
 def result = db.getById (1)
 def id = db.getRecordId ("hello world")
-println "found {$result} as record 1"
+println "found [${result.orElse("unknown")}] as record 1"
 
 db.dbShutdown ()
 
